0b6ac04915f35e14a5727dc3bf6d0e7a3feb15d8
Erfan Najimi <erfannajimi123@gmail.com>
2024-09-15 15:27:31 +0000

    Create flashcard model

--- flashcard/models.py
+++ flashcard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Flashcard(models.Model):
+    account = models.ForeignKey(User, on_delete=models.CASCADE)
+    group_name = models.CharField(max_length=60)
+    front = models.TextField()
+    back = models.TextField()
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        ordering = ["-added_on"]
+    
+    def __str__(self):
+        return f"{self.front}"
